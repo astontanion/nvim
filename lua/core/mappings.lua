3efd62a94f872af7850b482a6ec86d301d4e909c
astontanion <astontanion1123@gmail.com>
2023-07-16 21:23:43 +0100

    feat: set the leader to ' and add default movement mapping.

---
+++ lua/core/mappings.lua
@@ -0,0 +1,11 @@
+vim.g.mapleader = "'"
+vim.g.maplocalleader = "'"
+
+local map = vim.keymap.set
+
+map("n", "j", "v:count ? 'j' : 'gj'", { silent = true, expr = true } )
+map("n", "k", "v:count ? 'k' : 'gk'", { silent = true, expr = true })
+map("n", "n", "nzzzv")
+map("n", "N", "Nzzzv")
+map("v", "J", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '<-2<CR>gv=gv")
