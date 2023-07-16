cbad2a8d6d9c134330bc54e6e7b5626cd4b36a70
astontanion <astontanion1123@gmail.com>
2023-07-16 21:19:51 +0100

    feat: add default options.

---
+++ lua/core/options.lua
@@ -0,0 +1,29 @@
+-- disable netrw
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.opt.termguicolors = true
+
+vim.opt.backup = false
+vim.opt.swapfile = false
+
+vim.opt.autoindent = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+
+vim.opt.breakindent = true
+vim.opt.linebreak = true
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.autoread = true
+vim.opt.hidden = true
+vim.opt.wrap = true
+vim.opt.mouse = ""
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.scrolloff = 8
+vim.opt.updatetime = 50
