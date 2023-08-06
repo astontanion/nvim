f517c20b9604ecd8727802e1d896a05e5d57f552
astontanion <astontanion1123@gmail.com>
2023-08-06 14:11:17 +0100

    feat: update lualine.

--- lua/core/config/lualine.lua
+++ lua/core/config/lualine.lua
@@ -7,8 +7,24 @@ end
 lualine.setup({
 	options = {
 		icons_enabled = true,
-        theme = "auto",
-        section_separators = "",
-        component_separators = ""
+		theme = vim.g.colors_name,
+		section_separators = "",
+		component_separators = ""
 	},
+	sections = {
+		lualine_a = { 'mode' },
+		lualine_b = { 'filename' },
+		lualine_c = { 'diagnostics'},
+		lualine_x = { 'encoding', 'fileformat', 'filetype' },
+		lualine_y = { 'progress' },
+		lualine_z = { 'location' }
+	},
+	inactive_sections = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = {'filename'},
+		lualine_x = {'location'},
+		lualine_y = {},
+		lualine_z = {}
+	}
 })
