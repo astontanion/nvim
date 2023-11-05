0df8b36777555587cee7d3af3ee4d362e9527726
astontanion <astontanion1123@gmail.com>
2023-11-05 20:09:31 +0000

    feat: update lualine configuration for a minimal look.

--- lua/core/config/lualine.lua
+++ lua/core/config/lualine.lua
@@ -4,27 +4,94 @@ if not line_status then
 	return
 end
 
-lualine.setup({
+local conditions = {
+	buffer_not_empty = function()
+		return vim.fn.empty(vim.fn.expand('%:t')) ~= 1
+	end,
+	hide_in_width = function()
+		return vim.fn.winwidth(0) > 80
+	end,
+	check_git_workspace = function()
+		local filepath = vim.fn.expand('%:p:h')
+		local gitdir = vim.fn.finddir('.git', filepath .. ';')
+		return gitdir and #gitdir > 0 and #gitdir < #filepath
+	end,
+}
+
+local config = {
 	options = {
 		icons_enabled = true,
-		theme = vim.g.colors_name,
+		component_separators = "",
 		section_separators = "",
-		component_separators = ""
+		theme = vim.g.colors_name
 	},
 	sections = {
-		lualine_a = { 'mode' },
-		lualine_b = { 'filename' },
-		lualine_c = { 'diagnostics'},
-		lualine_x = { 'encoding', 'fileformat', 'filetype' },
-		lualine_y = { 'progress' },
-		lualine_z = { 'location' }
+		lualine_a = {},
+		lualine_b = {},
+		lualine_y = {},
+		lualine_z = {},
+		lualine_c = {},
+		lualine_x = {},
 	},
 	inactive_sections = {
 		lualine_a = {},
 		lualine_b = {},
-		lualine_c = {'filename'},
-		lualine_x = {'location'},
 		lualine_y = {},
-		lualine_z = {}
-	}
-})
+		lualine_z = {},
+		lualine_c = {},
+		lualine_x = {},
+	},
+}
+
+local function ins_left(component)
+	table.insert(config.sections.lualine_c, component)
+end
+
+local function ins_right(component)
+	table.insert(config.sections.lualine_x, component)
+end
+
+ins_left {
+	function()
+		return ''
+	end,
+	padding = { left = 1, right = 1 },
+}
+
+ins_left {
+	'filename',
+	path = 4,
+	color = { gui = 'bold' },
+	cond = conditions.buffer_not_empty,
+}
+
+ins_left {
+	'diagnostics', 
+	sources = { 'nvim_lsp' },
+	symbols = { error = ' ', warn = ' ', info = ' ' },
+}
+
+ins_left { 'location' }
+
+ins_left { 'progress' }
+
+ins_left {
+	function()
+		return '%='
+	end,
+}
+
+ins_right {
+	'o:encoding',
+	fmt = string.lower,
+	cond = conditions.hide_in_width,
+}
+
+ins_right {
+	'filetype',
+	fmt = string.lower,
+	icons_enabled = true,
+	padding = { left = 1, right = 1 },
+}
+
+lualine.setup(config)
