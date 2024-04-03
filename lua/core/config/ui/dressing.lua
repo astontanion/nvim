b91a90f563f91a6e2571bf32900e2c9ed8c88350
astontanion <astontanion1123@gmail.com>
2024-04-03 23:08:00 +0100

    feat: update dressing to center the input label.

--- lua/core/config/ui/dressing.lua
+++ lua/core/config/ui/dressing.lua
@@ -4,7 +4,11 @@ M.configure = function()
 	local dressing_status, dressing = pcall(require, "dressing")
 	if not dressing_status then return end
 
-	dressing.setup({})
+	dressing.setup({
+		input = {
+			title_pos = "center",
+		}
+	})
 end
 
 return M
