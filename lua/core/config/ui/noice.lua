c47a2d711601704f05ce4dbbf718c26fa8ce5867
astontanion <astontanion1123@gmail.com>
2024-04-27 15:30:23 +0100

    change: silence notice lsp logs.

--- lua/core/config/ui/noice.lua
+++ lua/core/config/ui/noice.lua
@@ -6,22 +6,168 @@ M.configure = function()
 	if not noice_status then return end
 
 	noice.setup({
-		lsp = {
-			-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-			override = {
-				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-				["vim.lsp.util.stylize_markdown"] = true,
-				["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+		cmdline = {
+			enabled = true,
+			view = "cmdline_popup",
+			opts = {},
+			format = {
+				cmdline = { pattern = "^:", icon = "", lang = "vim" },
+				search_down = { kind = "search", pattern = "^/", icon = " ", lang = "regex" },
+				search_up = { kind = "search", pattern = "^%?", icon = " ", lang = "regex" },
+				filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
+				lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, icon = "", lang = "lua" },
+				help = { pattern = "^:%s*he?l?p?%s+", icon = "" },
+				input = {},
 			},
 		},
-		-- you can enable a preset for easier configuration
+		messages = {
+			enabled = true,
+			view = "notify",
+			view_error = "notify",
+			view_warn = "notify",
+			view_history = "messages",
+			view_search = "virtualtext",
+		},
+		popupmenu = {
+			enabled = true,
+			backend = "nui",
+			kind_icons = {},
+		},
+		redirect = {
+			view = "popup",
+			filter = { event = "msg_show" },
+		},
+		commands = {
+			history = {
+				view = "split",
+				opts = { enter = true, format = "details" },
+				filter = {
+					any = {
+						{ event = "notify" },
+						{ error = true },
+						{ warning = true },
+						{ event = "msg_show", kind = { "" } },
+						{ event = "lsp", kind = "message" },
+					},
+				},
+			},
+			last = {
+				view = "popup",
+				opts = { enter = true, format = "details" },
+				filter = {
+					any = {
+						{ event = "notify" },
+						{ error = true },
+						{ warning = true },
+						{ event = "msg_show", kind = { "" } },
+						{ event = "lsp", kind = "message" },
+					},
+				},
+				filter_opts = { count = 1 },
+			},
+			errors = {
+				view = "popup",
+				opts = { enter = true, format = "details" },
+				filter = { error = true },
+				filter_opts = { reverse = true },
+			},
+		},
+		notify = {
+			enabled = true,
+			view = "notify",
+		},
+		lsp = {
+			progress = {
+				enabled = false,
+				format = "lsp_progress",
+				format_done = "lsp_progress_done",
+				throttle = 1000 / 30,
+				view = "mini",
+			},
+			override = {
+				["vim.lsp.util.convert_input_to_markdown_lines"] = false,
+				["vim.lsp.util.stylize_markdown"] = false,
+				["cmp.entry.get_documentation"] = false,
+			},
+			hover = {
+				enabled = true,
+				silent = false,
+				view = nil,
+				opts = {},
+			},
+			signature = {
+				enabled = true,
+				auto_open = {
+					enabled = true,
+					trigger = true,
+					luasnip = true,
+					throttle = 50,
+				},
+				view = nil,
+				opts = {},
+			},
+			message = {
+				enabled = true,
+				view = "notify",
+				opts = {},
+			},
+			documentation = {
+				view = "hover",
+				opts = {
+					lang = "markdown",
+					replace = true,
+					render = "plain",
+					format = { "{message}" },
+					win_options = { concealcursor = "n", conceallevel = 3 },
+				},
+			},
+		},
+		markdown = {
+			hover = {
+				["|(%S-)|"] = vim.cmd.help,
+				["%[.-%]%((%S-)%)"] = require("noice.util").open,
+			},
+			highlights = {
+				["|%S-|"] = "@text.reference",
+				["@%S+"] = "@parameter",
+				["^%s*(Parameters:)"] = "@text.title",
+				["^%s*(Return:)"] = "@text.title",
+				["^%s*(See also:)"] = "@text.title",
+				["{%S-}"] = "@parameter",
+			},
+		},
+		health = {
+			checker = true,
+		},
+		smart_move = {
+			enabled = true,
+			excluded_filetypes = { "cmp_menu", "cmp_docs", "notify" },
+		},
 		presets = {
-			bottom_search = true, -- use a classic bottom cmdline for search
-			command_palette = true, -- position the cmdline and popupmenu together
-			long_message_to_split = true, -- long messages will be sent to a split
-			inc_rename = false, -- enables an input dialog for inc-rename.nvim
-			lsp_doc_border = false, -- add a border to hover docs and signature help
-		}
+			bottom_search = false,
+			command_palette = false,
+			long_message_to_split = false,
+			inc_rename = false,
+			lsp_doc_border = false,
+		},
+		throttle = 1000 / 30,
+		views = {},
+		routes = {
+			{
+				view = "mini",
+				filter = { event = "msg_showmode" }
+			},
+			{
+				filter = {
+					event = "msg_show",
+					kind = "",
+					find = "written"
+				},
+				opts = { skip = true }
+			}
+		},
+		status = {},
+		format = {},
 	})
 end
 
